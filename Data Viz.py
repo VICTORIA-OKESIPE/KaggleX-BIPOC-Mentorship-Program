523ac5bc837397e1ea880dce6563439812e35fc9
VICTORIA OKESIPE <vokesipe888@stu.ui.edu.ng>
2023-07-27 05:16:06 +0100

    Update Data Viz.py

--- Data Viz.py
+++ Data Viz.py
@@ -26,3 +26,10 @@ plt.legend()
 
 # Show the plot()
 plt.show()
+---
+
+# Scatter plot of avg_price vs. nb_sold with title
+avocados.plot(x="nb_sold", y="avg_price", kind="scatter", title="Number of avocados sold vs. average price")
+
+# Show the plot
+plt.show()
