e3e9a3515011b6fb8531211d672169deb7b246fe
VICTORIA OKESIPE <vokesipe888@stu.ui.edu.ng>
2023-07-27 05:33:43 +0100

    Update Data Viz.py

--- Data Viz.py
+++ Data Viz.py
@@ -46,3 +46,16 @@ plt.legend(["conventional", "organic"])
 
 # Show the plot
 plt.show()
+---
+
+# Modify bins to 20
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(bins=20,alpha=0.5)
+
+# Modify bins to 20
+avocados[avocados["type"] == "organic"]["avg_price"].hist(bins=20,alpha=0.5)
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()
