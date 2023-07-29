81398306e766eb06b9bee710fac2abbc8e35345e
VICTORIA OKESIPE <vokesipe888@stu.ui.edu.ng>
2023-07-29 17:13:23 +0100

    Update Data Viz.py

--- Data Viz.py
+++ Data Viz.py
@@ -1,5 +1,5 @@
 # Import matplotlib.pyplot with alias plt
-import matplotlib.pyplot as pltttttt
+import matplotlib.pyplot as plttttttt
 
 # Look at the first few rows of data
 print(avocados.head())
